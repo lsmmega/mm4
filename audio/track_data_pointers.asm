ffde532c51c344e83108e980e5cd7c74bad00046
lsmmega <pogmegalsm@outlook.com>
2024-06-23 19:58:37 +0800

    create list

---
+++ audio/track_data_pointers.asm
@@ -0,0 +1,160 @@
+;track_total
+	db (track_data_end-track_data_start)/2
+
+;instruments_pointers
+	;dh instruments_data
+	;dl instruments_data
+
+track_data_start
+;music
+	;dh music_bright
+	;dl music_bright
+	;dh music_toad
+	;dl music_toad
+	;dh music_ring
+	;dl music_ring
+	;dh music_drill
+	;dl music_drill
+	;dh music_pharaoh
+	;dl music_pharaoh
+	;dh music_dive
+	;dl music_dive
+	;dh music_skull
+	;dl music_skull
+	;dh music_dust
+	;dl music_dust
+	;dh music_cossack1
+	;dl music_cossack1
+	;dh music_wily1
+	;dl music_wily1
+	;dh music_opening1
+	;dl music_opening1
+	;dh music_ending
+	;dl music_ending
+	;dh music_stage_clear
+	;dl music_stage_clear
+	;dh music_pass_word
+	;dl music_pass_word
+	;dh music_game_over
+	;dl music_game_over
+	;dh music_game_start
+	;dl music_game_start
+	;dh music_stage_select
+	;dl music_stage_select
+	;dh music_boss
+	;dl music_boss
+	;dh music_title
+	;dl music_title
+	;dh music_staff_roll
+	;dl music_staff_roll
+	;dh music_get_weapon
+	;dl music_get_weapon
+	;dh music_cossack3
+	;dl music_cossack3
+	;dh music_wily3
+	;dl music_wily3
+
+;sfx
+	;dh sfx_flash_stopper
+	;dl sfx_flash_stopper
+	;dh sfx_rain_flush
+	;dl sfx_rain_flush
+	;dh sfx_ring_boomerang
+	;dl sfx_ring_boomerang
+	;dh sfx_drill_bomb
+	;dl sfx_drill_bomb
+	;dh sfx_uncharged_pharaoh_shot
+	;dl sfx_uncharged_pharaoh_shot
+	;dh sfx_lighting
+	;dl sfx_lighting
+	;dh sfx_routine_map
+	;dl sfx_routine_map
+	;dh sfx_water
+	;dl sfx_water
+	;dh sfx_1up
+	;dl sfx_1up
+	;dh sfx_unused20
+	;dl sfx_unused20
+	;dh sfx_shoot
+	;dl sfx_shoot
+	;dh sfx_charging_buster
+	;dl sfx_charging_buster
+	;dh sfx_landing
+	;dl sfx_landing
+	;dh sfx_damage_mm
+	;dl sfx_damage_mm
+	;dh sfx_death
+	;dl sfx_death
+	;dh sfx_destroy_enemy
+	;dl sfx_destroy_enemy
+	;dh sfx_fully_charging_buster
+	;dl sfx_fully_charging_buster
+	;dh sfx_explosion
+	;dl sfx_explosion
+	;dh sfx_gain_bar
+	;dl sfx_gain_bar
+	;dh sfx_select_robot_master
+	;dl sfx_select_robot_master
+	;dh sfx_shoot_recoil
+	;dl sfx_shoot_recoil
+	;dh sfx_boss_door
+	;dl sfx_boss_door
+	;dh sfx_password_wrong
+	;dl sfx_password_wrong
+	;dh sfx_change_robot_master
+	;dl sfx_change_robot_master
+	;dh sfx_password_correct
+	;dl sfx_password_correct
+	;dh sfx_menu
+	;dl sfx_menu
+	;dh sfx_battan
+	;dl sfx_battan
+	;dh sfx_wire
+	;dl sfx_wire
+	;dh sfx_teleport_in
+	;dl sfx_teleport_in
+	;dh sfx_unused34
+	;dl sfx_unused34
+	;dh sfx_get_weapon_text
+	;dl sfx_get_weapon_text
+	;dh sfx_lab_alert
+	;dl sfx_lab_alert
+	;dh sfx_wily_castle_explosion
+	;dl sfx_wily_castle_explosion
+	;dh sfx_unused38
+	;dl sfx_unused38
+	;dh sfx_yoku_block
+	;dl sfx_yoku_block
+
+;music
+	;dh music_get_items
+	;dl music_get_items
+	;dh music_wily_map
+	;dl music_wily_map
+	;dh music_cossack_map
+	;dl music_cossack_map
+
+;sfx
+	;dh sfx_fully_charged_shoot
+	;dl sfx_fully_charged_shoot
+	;dh sfx_fully_pharaoh_shot
+	;dl sfx_fully_pharaoh_shot
+
+;music
+	;dh music_proto_whistle
+	;dl music_proto_whistle
+
+;sfx
+	;dh sfx_ufo
+	;dl sfx_ufo
+	;dh sfx_teleport_out
+	;dl sfx_teleport_out
+
+;music
+	;dh music_opening2
+	;dl music_opening2
+	;dh music_last_boss
+	;dl music_last_boss
+	;dh music_all_stage_clear
+	;dl music_all_stage_clear
+track_data_end

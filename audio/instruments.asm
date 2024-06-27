6fdd30d75b5d7de47c280c194f4143f62ec29afc
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-27 10:09:41 +0800

    Update audio

---
+++ audio/instruments.asm
@@ -0,0 +1,710 @@
+instruments_data:
+;instrument_$00
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $00
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$01
+	attack $1f
+	decay $15
+	sustain_volume_rate $d
+	release $01
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$02
+	attack $1f
+	decay $1e
+	sustain_volume_rate $3
+	release $07
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $80
+
+;instrument_$03
+	attack $1f
+	decay $19
+	sustain_volume_rate $3
+	release $09
+	vibrato_rate $e4
+	pitch_vibrato $05
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$04
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $94
+	pitch_vibrato $7f
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$05
+	attack $1a
+	decay $1f
+	sustain_volume_rate $1
+	release $1c
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$06
+	attack $1f
+	decay $11
+	sustain_volume_rate $a
+	release $01
+	vibrato_rate $e2
+	pitch_vibrato $04
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$07
+	attack $1f
+	decay $1a
+	sustain_volume_rate $2
+	release $17
+	vibrato_rate $92
+	pitch_vibrato $00
+	volume_vibrato $26
+	noise_flags $00
+
+;instrument_$08
+	attack $1f
+	decay $18
+	sustain_volume_rate $4
+	release $06
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$09
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $11
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$0a
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $ff
+	pitch_vibrato $03
+	volume_vibrato $39
+	noise_flags $00
+
+;instrument_$0b
+	attack $1f
+	decay $1c
+	sustain_volume_rate $6
+	release $03
+	vibrato_rate $ff
+	pitch_vibrato $04
+	volume_vibrato $25
+	noise_flags $00
+
+;instrument_$0c
+	attack $1f
+	decay $00
+	sustain_volume_rate $4
+	release $09
+	vibrato_rate $e3
+	pitch_vibrato $02
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$0d
+	attack $1f
+	decay $1d
+	sustain_volume_rate $3
+	release $11
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$0e
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $94
+	pitch_vibrato $7f
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$0f
+	attack $18
+	decay $1b
+	sustain_volume_rate $4
+	release $0e
+	vibrato_rate $84
+	pitch_vibrato $7f
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$10
+	attack $1f
+	decay $00
+	sustain_volume_rate $4
+	release $07
+	vibrato_rate $e3
+	pitch_vibrato $02
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$11
+	attack $1b
+	decay $01
+	sustain_volume_rate $5
+	release $16
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$12
+	attack $1f
+	decay $0c
+	sustain_volume_rate $f
+	release $02
+	vibrato_rate $a3
+	pitch_vibrato $00
+	volume_vibrato $09
+	noise_flags $00
+
+;instrument_$13
+	attack $1f
+	decay $1a
+	sustain_volume_rate $2
+	release $0d
+	vibrato_rate $dc
+	pitch_vibrato $06
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$14
+	attack $1f
+	decay $17
+	sustain_volume_rate $2
+	release $05
+	vibrato_rate $98
+	pitch_vibrato $02
+	volume_vibrato $2c
+	noise_flags $00
+
+;instrument_$15
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $13
+	vibrato_rate $d0
+	pitch_vibrato $03
+	volume_vibrato $7f
+	noise_flags $00
+
+;instrument_$16
+	attack $19
+	decay $13
+	sustain_volume_rate $2
+	release $18
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$17
+	attack $1f
+	decay $00
+	sustain_volume_rate $3
+	release $18
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$18
+	attack $1f
+	decay $01
+	sustain_volume_rate $d
+	release $09
+	vibrato_rate $99
+	pitch_vibrato $10
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$19
+	attack $1a
+	decay $1f
+	sustain_volume_rate $2
+	release $01
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$1a
+	attack $1f
+	decay $16
+	sustain_volume_rate $3
+	release $14
+	vibrato_rate $ab
+	pitch_vibrato $02
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$1b
+	attack $1f
+	decay $1b
+	sustain_volume_rate $7
+	release $05
+	vibrato_rate $c9
+	pitch_vibrato $00
+	volume_vibrato $38
+	noise_flags $00
+
+;instrument_$1c
+	attack $1f
+	decay $1f
+	sustain_volume_rate $4
+	release $09
+	vibrato_rate $00
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$1d
+	attack $1f
+	decay $1d
+	sustain_volume_rate $4
+	release $0c
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$1e
+	attack $1f
+	decay $19
+	sustain_volume_rate $5
+	release $02
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$1f
+	attack $17
+	decay $02
+	sustain_volume_rate $1
+	release $0f
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$20
+	attack $1e
+	decay $18
+	sustain_volume_rate $3
+	release $12
+	vibrato_rate $00
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$21
+	attack $1f
+	decay $07
+	sustain_volume_rate $d
+	release $16
+	vibrato_rate $82
+	pitch_vibrato $14
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$22
+	attack $1f
+	decay $19
+	sustain_volume_rate $4
+	release $04
+	vibrato_rate $e0
+	pitch_vibrato $05
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$23
+	attack $1f
+	decay $1e
+	sustain_volume_rate $05
+	release $0a
+	vibrato_rate $aa
+	pitch_vibrato $00
+	volume_vibrato $4b
+	noise_flags $00
+
+;instrument_$24
+	attack $1f
+	decay $18
+	sustain_volume_rate $6
+	release $05
+	vibrato_rate $00
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$25
+	attack $00
+	decay $00
+	sustain_volume_rate $7
+	release $00
+	vibrato_rate $00
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$26
+	attack $1a
+	decay $1f
+	sustain_volume_rate $2
+	release $07
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$27
+	attack $00
+	decay $00
+	sustain_volume_rate $7
+	release $00
+	vibrato_rate $00
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$28
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $00
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$29
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $ff
+	pitch_vibrato $02
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$2a
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $92
+	pitch_vibrato $7f
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$2b
+	attack $1f
+	decay $1c
+	sustain_volume_rate $3
+	release $15
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$2c
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $ff
+	pitch_vibrato $4c
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$2d
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $99
+	pitch_vibrato $7f
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$2e
+	attack $1d
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$2f
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $b7
+	pitch_vibrato $27
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$30
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $0f
+	vibrato_rate $d7
+	pitch_vibrato $09
+	volume_vibrato $7f
+	noise_flags $00
+
+;instrument_$31
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $a6
+	pitch_vibrato $7f
+	volume_vibrato $00
+	noise_flags $80
+
+;instrument_$32
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $80
+
+;instrument_$33
+	attack $1f
+	decay $01
+	sustain_volume_rate $0
+	release $05
+	vibrato_rate $e1
+	pitch_vibrato $0d
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$34
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $c1
+	pitch_vibrato $04
+	volume_vibrato $7f
+	noise_flags $00
+
+;instrument_$35
+	attack $1c
+	decay $13
+	sustain_volume_rate $e
+	release $1f
+	vibrato_rate $ff
+	pitch_vibrato $7f
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$36
+	attack $1f
+	decay $01
+	sustain_volume_rate $f
+	release $0f
+	vibrato_rate $e3
+	pitch_vibrato $7f
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$37
+	attack $1e
+	decay $10
+	sustain_volume_rate $0
+	release $0e
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$38
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $ff
+	pitch_vibrato $7f
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$39
+	attack $1f
+	decay $17
+	sustain_volume_rate $f
+	release $16
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$3a
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $16
+	vibrato_rate $80
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$3b
+	attack $1f
+	decay $01
+	sustain_volume_rate $0
+	release $05
+	vibrato_rate $e1
+	pitch_vibrato $0d
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$3c
+	attack $1f
+	decay $1a
+	sustain_volume_rate $5
+	release $07
+	vibrato_rate $cf
+	pitch_vibrato $36
+	volume_vibrato $00
+	noise_flags $80
+
+;instrument_$3d
+	attack $1d
+	decay $04
+	sustain_volume_rate $d
+	release $12
+	vibrato_rate $00
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$3e
+	attack $11
+	decay $12
+	sustain_volume_rate $c
+	release $04
+	vibrato_rate $00
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$3f
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $d0
+	pitch_vibrato $16
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$40
+	attack $1c
+	decay $1f
+	sustain_volume_rate $0
+	release $07
+	vibrato_rate $ff
+	pitch_vibrato $7f
+	volume_vibrato $36
+	noise_flags $00
+
+;instrument_$41
+	attack $19
+	decay $0b
+	sustain_volume_rate $0
+	release $12
+	vibrato_rate $ff
+	pitch_vibrato $7f
+	volume_vibrato $36
+	noise_flags $80
+
+;instrument_$42
+	attack $1f
+	decay $12
+	sustain_volume_rate $b
+	release $1e
+	vibrato_rate $ff
+	pitch_vibrato $7f
+	volume_vibrato $51
+	noise_flags $80
+
+;instrument_$43
+	attack $17
+	decay $1f
+	sustain_volume_rate $0
+	release $02
+	vibrato_rate $ff
+	pitch_vibrato $7f
+	volume_vibrato $5a
+	noise_flags $00
+
+;instrument_$44
+	attack $12
+	decay $02
+	sustain_volume_rate $5
+	release $05
+	vibrato_rate $00
+	pitch_vibrato $00
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$45
+	attack $1f
+	decay $1f
+	sustain_volume_rate $0
+	release $1f
+	vibrato_rate $ff
+	pitch_vibrato $19
+	volume_vibrato $00
+	noise_flags $00
+
+;instrument_$46
+	attack $17
+	decay $14
+	sustain_volume_rate $0
+	release $07
+	vibrato_rate $ca
+	pitch_vibrato $54
+	volume_vibrato $00
+	noise_flags $00

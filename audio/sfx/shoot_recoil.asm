64a820b85438d84ecd09d6894bcb88a0450fe095
lsmmega <pogmegalsm@outlook.com>
2024-06-29 12:09:49 +0800

    Create shoot_recoil.asm

---
+++ audio/sfx/shoot_recoil.asm
@@ -0,0 +1,22 @@
+sfx_shoot_recoil:
+;1
+	sfx_priority $09
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $04
+	sfx_channel_flags $6
+	sfx_channel_flags_bits = $6
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $28
+	sfx_pulse2_duty_cycle 0
+	sfx_pulse2_volume $f
+	sfx_pulse2_note A_6
+	sfx_triangle_command_flags $05
+	sfx_triangle_command_flags_bits = $05
+	sfx_triangle_instrument $28
+	sfx_triangle_fraction $af
+	sfx_triangle_note A_7
+	sfx_end

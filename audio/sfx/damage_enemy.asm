fbeba43a162a287ba28429111047464179512cbb
lsmmega <pogmegalsm@outlook.com>
2024-06-29 12:09:36 +0800

    Create damage_enemy.asm

---
+++ audio/sfx/damage_enemy.asm
@@ -0,0 +1,23 @@
+sfx_damage_enemy:
+;1
+	sfx_priority $09
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $c8
+	sfx_global_transpose_note = $00
+	sfx_frames $1c
+	sfx_channel_flags $c
+	sfx_channel_flags_bits = $c
+	sfx_triangle_command_flags $0d
+	sfx_triangle_command_flags_bits = $0d
+	sfx_triangle_instrument $00
+	sfx_triangle_fraction $7d
+	sfx_triangle_pitch_slide $ff
+	sfx_triangle_note F_1
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $32
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $ff
+	sfx_noise_note 13
+	sfx_end

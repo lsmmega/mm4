e4a0e8634385932821885b4a6006e07e16445ac2
lsmmega <pogmegalsm@outlook.com>
2024-06-29 12:10:03 +0800

    update WIP

--- audio/track_data_pointers.asm
+++ audio/track_data_pointers.asm
@@ -31,8 +31,8 @@ track_data_start
 	;dl music_opening1
 	;dh music_ending
 	;dl music_ending
-	;dh music_stage_clear
-	;dl music_stage_clear
+	dh music_stage_clear
+	dl music_stage_clear
 	;dh music_pass_word
 	;dl music_pass_word
 	;dh music_game_over
@@ -67,40 +67,40 @@ track_data_start
 	;dl sfx_uncharged_pharaoh_shot
 	;dh sfx_lighting
 	;dl sfx_lighting
-	;dh sfx_routine_map
-	;dl sfx_routine_map
-	;dh sfx_water
-	;dl sfx_water
-	;dh sfx_1up
-	;dl sfx_1up
+	dh sfx_routine_map
+	dl sfx_routine_map
+	dh sfx_water
+	dl sfx_water
+	dh sfx_1up
+	dl sfx_1up
 	;dh sfx_unused20
 	;dl sfx_unused20
-	;dh sfx_shoot
-	;dl sfx_shoot
+	dh sfx_shoot
+	dl sfx_shoot
 	;dh sfx_charging_buster
 	;dl sfx_charging_buster
-	;dh sfx_landing
-	;dl sfx_landing
-	;dh sfx_damage_mm
-	;dl sfx_damage_mm
-	;dh sfx_death
-	;dl sfx_death
-	;dh sfx_destroy_enemy
-	;dl sfx_destroy_enemy
+	dh sfx_landing
+	dl sfx_landing
+	dh sfx_damage_mm
+	dl sfx_damage_mm
+	dh sfx_death
+	dl sfx_death
+	dh sfx_damage_enemy
+	dl sfx_damage_enemy
 	;dh sfx_fully_charging_buster
 	;dl sfx_fully_charging_buster
-	;dh sfx_explosion
-	;dl sfx_explosion
-	;dh sfx_gain_bar
-	;dl sfx_gain_bar
+	dh sfx_explosion
+	dl sfx_explosion
+	dh sfx_gain_bar
+	dl sfx_gain_bar
 	;dh sfx_select_robot_master
 	;dl sfx_select_robot_master
-	;dh sfx_shoot_recoil
-	;dl sfx_shoot_recoil
-	;dh sfx_boss_door
-	;dl sfx_boss_door
-	;dh sfx_password_wrong
-	;dl sfx_password_wrong
+	dh sfx_shoot_recoil
+	dl sfx_shoot_recoil
+	dh sfx_boss_door
+	dl sfx_boss_door
+	dh sfx_password_wrong
+	dl sfx_password_wrong
 	;dh sfx_change_robot_master
 	;dl sfx_change_robot_master
 	;dh sfx_password_correct
@@ -111,8 +111,8 @@ track_data_start
 	;dl sfx_battan
 	;dh sfx_wire
 	;dl sfx_wire
-	;dh sfx_teleport_in
-	;dl sfx_teleport_in
+	dh sfx_teleport_in
+	dl sfx_teleport_in
 	;dh sfx_unused34
 	;dl sfx_unused34
 	;dh sfx_get_weapon_text
@@ -123,8 +123,8 @@ track_data_start
 	;dl sfx_wily_castle_explosion
 	;dh sfx_unused38
 	;dl sfx_unused38
-	;dh sfx_yoku_block
-	;dl sfx_yoku_block
+	dh sfx_yoku_block
+	dl sfx_yoku_block
 
 ;music
 	;dh music_get_items
@@ -145,14 +145,18 @@ track_data_start
 	;dl music_proto_whistle
 
 ;sfx
-	;dh sfx_ufo
-	;dl sfx_ufo
-	;dh sfx_teleport_out
-	;dl sfx_teleport_out
+	dh sfx_ufo
+	dl sfx_ufo
+	dh sfx_teleport_out
+	dl sfx_teleport_out
+	;dh sfx_unknown42
+	;dl sfx_unknown42
 
 ;music
 	;dh music_opening2
 	;dl music_opening2
+	dh 0
+	dl 0
 	;dh music_last_boss
 	;dl music_last_boss
 	;dh music_all_stage_clear

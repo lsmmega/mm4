b6ce278cbedbdb430c0dea9501c85d77092ed2eb
lsmmega <pogmegalsm@outlook.com>
2024-06-29 12:31:26 +0800

    Create select_robot_master.asm

---
+++ audio/sfx/select_robot_master.asm
@@ -0,0 +1,49 @@
+incsrc .\scripts\audio.asm
+sfx_select_robot_master:
+;1
+	sfx_priority $08
+	sfx_global_flags $6
+	sfx_global_flags_bits = $6
+	sfx_global_note_sustain_length $bf
+	sfx_global_transpose_note = $03
+	sfx_frames $04
+	sfx_channel_flags $3
+	sfx_channel_flags_bits = $3
+	sfx_pulse1_command_flags $17
+	sfx_pulse1_command_flags_bits = $17
+	sfx_pulse1_instrument $00
+	sfx_pulse1_duty_cycle 3
+	sfx_pulse1_volume $f
+	sfx_pulse1_pitch_tune $ff
+	sfx_pulse1_note G#5
+	sfx_pulse2_command_flags $17
+	sfx_pulse2_command_flags_bits = $17
+	sfx_pulse2_instrument $05
+	sfx_pulse2_duty_cycle 3
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_tune $00
+	sfx_pulse2_note G#5
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $29
+	sfx_global_transpose_note = $03
+	sfx_frames $1d
+	sfx_channel_flags $3
+	sfx_channel_flags_bits = $3
+	sfx_pulse1_command_flags $17
+	sfx_pulse1_command_flags_bits = $17
+	sfx_pulse1_instrument $02
+	sfx_pulse1_duty_cycle 3
+	sfx_pulse1_volume $e
+	sfx_pulse1_pitch_tune $fe
+	sfx_pulse1_note E_6_
+	sfx_pulse2_command_flags $17
+	sfx_pulse2_command_flags_bits = $17
+	sfx_pulse2_instrument $02
+	sfx_pulse2_duty_cycle 0
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_tune $00
+	sfx_pulse2_note E_6_
+	sfx_end

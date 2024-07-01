cced6333248233f44397b1fdbbbb7c4bf427760b
lsmmega <pogmegalsm@outlook.com>
2024-07-01 20:44:50 +0800

    Create fully_pharaoh_shot.asm

---
+++ audio/sfx/fully_pharaoh_shot.asm
@@ -0,0 +1,17 @@
+sfx_fully_pharaoh_shot:
+;1
+	sfx_priority $06
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $2f
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $3e
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $9a
+	sfx_noise_note 15
+	sfx_end

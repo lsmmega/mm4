acd506cca1b42b62f6351563f4a3fc4606e21267
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-07-01 13:58:50 +0800

    Update sfx

---
+++ audio/sfx/battan.asm
@@ -0,0 +1,51 @@
+sfx_battan:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $02
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $2c
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $6e
+	sfx_pulse2_note D#7
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $3c
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $4a
+	sfx_noise_note 8
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $04
+	sfx_channel_flags $0
+	sfx_channel_flags_bits = $0
+
+;3
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $37
+	sfx_global_transpose_note = $00
+	sfx_frames $09
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $b
+	sfx_pulse2_note D#7
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $07
+	sfx_noise_volume $f
+	sfx_noise_note 12
+	sfx_end

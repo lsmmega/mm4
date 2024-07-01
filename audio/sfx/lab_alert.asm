1e58a91068b185f79ade7d82418b969c40abbaa9
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-07-01 17:19:05 +0800

    Update sfx

---
+++ audio/sfx/lab_alert.asm
@@ -0,0 +1,25 @@
+sfx_lab_alert:
+;1
+	sfx_priority $07
+	sfx_global_flags $6
+	sfx_global_flags_bits = $6
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose $f1
+	sfx_global_transpose_note = $f1
+	sfx_frames $30
+	sfx_channel_flags $6
+	sfx_channel_flags_bits = $6
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $28
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $8b
+	sfx_pulse2_note F#1
+	sfx_triangle_command_flags $0d
+	sfx_triangle_command_flags_bits = $0d
+	sfx_triangle_instrument $28
+	sfx_triangle_fraction $ff
+	sfx_triangle_pitch_slide $8d
+	sfx_triangle_note F#2
+	sfx_end

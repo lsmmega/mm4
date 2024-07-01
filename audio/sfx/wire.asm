7e837f31c2b457c3d90d93c73079955a6049c8c0
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-07-01 14:06:56 +0800

    Update sfx

---
+++ audio/sfx/wire.asm
@@ -0,0 +1,21 @@
+sfx_wire:
+;1
+	sfx_priority $06
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $14
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $05
+	sfx_pulse2_command_flags_bits = $05
+	sfx_pulse2_instrument $0a
+	sfx_pulse2_volume $f
+	sfx_pulse2_note C#6
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $0a
+	sfx_noise_volume $f
+	sfx_noise_note 12
+	sfx_end

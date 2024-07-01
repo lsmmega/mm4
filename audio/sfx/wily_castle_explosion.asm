995a62aa39eb6a9cd44bcb21d8cc6dc975a8a2c9
lsmmega <pogmegalsm@outlook.com>
2024-07-01 20:44:52 +0800

    Create wily_castle_explosion.asm

---
+++ audio/sfx/wily_castle_explosion.asm
@@ -0,0 +1,47 @@
+sfx_wily_castle_explosion:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $dc
+	sfx_global_transpose_note = $00
+	sfx_frames $05
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $3b
+	sfx_pulse2_duty_cycle 3
+	sfx_pulse2_volume $f
+	sfx_pulse2_note C_1
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $3b
+	sfx_noise_volume $b
+	sfx_noise_note 2
+
+;2
+	sfx_global_flags $0
+	sfx_global_flags_bits = $0
+	sfx_global_transpose_note = $00
+	sfx_frames $01
+	sfx_channel_flags $0
+	sfx_channel_flags_bits = $0
+
+;3
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $53
+	sfx_global_transpose_note = $00
+	sfx_frames $56
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $08
+	sfx_pulse2_command_flags_bits = $08
+	sfx_pulse2_pitch_slide $00
+	sfx_pulse2_note C#1
+	sfx_noise_command_flags $04
+	sfx_noise_command_flags_bits = $04
+	sfx_noise_volume $f
+	sfx_noise_note 0
+	sfx_end

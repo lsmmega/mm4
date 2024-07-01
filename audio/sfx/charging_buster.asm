d35a5de8807d126c4755443c1b7f21703796cedb
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-07-01 11:29:31 +0800

    Update sfx

---
+++ audio/sfx/charging_buster.asm
@@ -0,0 +1,116 @@
+sfx_charging_buster:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $08
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $1f
+	sfx_pulse2_command_flags_bits = $1f
+	sfx_pulse2_instrument $45
+	sfx_pulse2_duty_cycle 1
+	sfx_pulse2_volume $c
+	sfx_pulse2_pitch_slide $c8
+	sfx_pulse2_pitch_tune $00
+	sfx_pulse2_note B_2
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note C_3
+
+;3
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note C#3
+
+;4
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note D_3
+
+;5
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note D#3
+
+;6
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note E_3
+
+;7
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note F_3
+
+;8
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note F#3
+
+;9
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $a
+	sfx_pulse2_note G_3
+	sfx_end

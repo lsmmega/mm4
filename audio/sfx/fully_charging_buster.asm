63ea10053ac2fed57a2c5b2c3bea9644e76d592d
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-07-01 13:23:11 +0800

    Update sfx

---
+++ audio/sfx/fully_charging_buster.asm
@@ -0,0 +1,19 @@
+sfx_fully_charging_buster:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $0a
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $1f
+	sfx_pulse2_command_flags_bits = $1f
+	sfx_pulse2_instrument $45
+	sfx_pulse2_duty_cycle 1
+	sfx_pulse2_volume $a
+	sfx_pulse2_pitch_slide $0b
+	sfx_pulse2_pitch_tune $ec
+	sfx_pulse2_note C_4
+	sfx_end

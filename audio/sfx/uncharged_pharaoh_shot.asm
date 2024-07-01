d9ed13cd290cfcf0b7d2c956eafcff293e1b483b
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-07-01 09:55:37 +0800

    Update sfx

---
+++ audio/sfx/uncharged_pharaoh_shot.asm
@@ -0,0 +1,23 @@
+sfx_uncharged_pharaoh_shot:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $40
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $43
+	sfx_pulse2_duty_cycle 0
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $b4
+	sfx_pulse2_note D_2
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $43
+	sfx_noise_volume $e
+	sfx_noise_note 12
+	sfx_end

0d437285d0039c4e92a97a6a2265293f7549114c
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-07-01 09:45:19 +0800

    Update sfx

---
+++ audio/sfx/ring_boomerang.asm
@@ -0,0 +1,41 @@
+sfx_ring_boomerang:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $46
+	sfx_global_transpose_note = $00
+	sfx_frames $04
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0d
+	sfx_pulse2_command_flags_bits = $0d
+	sfx_pulse2_instrument $15
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $7f
+	sfx_pulse2_note C_7
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $38
+	sfx_noise_volume $b
+	sfx_noise_note 12
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $20
+	sfx_global_transpose_note = $00
+	sfx_frames $0d
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $09
+	sfx_pulse2_command_flags_bits = $09
+	sfx_pulse2_instrument $14
+	sfx_pulse2_pitch_slide $00
+	sfx_pulse2_note D#7
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $3d
+	sfx_noise_volume $f
+	sfx_noise_note 13
+	sfx_end

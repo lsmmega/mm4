b5f646c7235127b420ede81089d0b0c8338d7bd6
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-07-01 13:30:20 +0800

    Update sfx

---
+++ audio/sfx/change_robot_master.asm
@@ -0,0 +1,17 @@
+sfx_change_robot_master:
+;1
+	sfx_priority $06
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $05
+	sfx_global_transpose_note = $00
+	sfx_frames $0a
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $2b
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_note A_3
+	sfx_end

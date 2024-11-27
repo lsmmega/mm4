6e61f524092a7db0e6848c5d7d9d1cee1bcdd171
lsmmega <pogmegalsm@outlook.com>
2024-11-27 22:32:51 +0800

    add gfx

---
+++ gfx.asm
@@ -0,0 +1,26 @@
+.SEGMENT "GFX00"
+.INCBIN "gfx/gfx.bin", $0, $10000
+
+.SEGMENT "GFX01"
+.INCBIN "gfx/gfx.bin", $10000, $4000
+
+.SEGMENT "GFX02"
+.INCBIN "gfx/gfx.bin", $14000, $10000
+
+.SEGMENT "GFX03"
+.INCBIN "gfx/gfx.bin", $24000, $6000
+
+.SEGMENT "GFX04"
+.INCBIN "gfx/gfx.bin", $2A000, $2000
+
+.SEGMENT "GFX05"
+.INCBIN "gfx/gfx.bin", $2C000, $800
+
+.SEGMENT "GFX06"
+.INCBIN "gfx/gfx.bin", $2C800, $F00
+
+.SEGMENT "GFX07"
+.INCBIN "gfx/gfx.bin", $2D700, $800
+
+.SEGMENT "GFX08"
+.INCBIN "gfx/gfx.bin", $2DF00, $F00

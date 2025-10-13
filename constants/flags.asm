8df81dfd4771cf3115ec2faeae9fefc524191406
lsmmega <pogmegalsm@outlook.com>
2025-10-13 15:46:03 +0800

    Update flags.asm

--- constants/flags.asm
+++ constants/flags.asm
@@ -8,16 +8,6 @@
 	wily_3_clear    = 1 << 6
 	wily_4_clear    = 1 << 7
 
-;regular stage clear flags.
-	stage_brightman_clear  = 1 << 0
-	stage_toadman_clear    = 1 << 1
-	stage_drillman_clear   = 1 << 2
-	stage_pharaohman_clear = 1 << 3
-	stage_ringman_clear    = 1 << 4
-	stage_dustman_clear    = 1 << 5
-	stage_diveman_clear    = 1 << 6
-	stage_skullman_clear   = 1 << 7
-
 ;rematch flags.
 	rematch_brightman_done  = 1 << 0
 	rematch_toadman_done    = 1 << 1
@@ -27,3 +17,13 @@
 	rematch_dustman_done    = 1 << 5
 	rematch_diveman_done    = 1 << 6
 	rematch_skullman_done   = 1 << 7
+
+;regular stage clear flags.
+	stage_brightman_clear  = 1 << 0
+	stage_toadman_clear    = 1 << 1
+	stage_drillman_clear   = 1 << 2
+	stage_pharaohman_clear = 1 << 3
+	stage_ringman_clear    = 1 << 4
+	stage_dustman_clear    = 1 << 5
+	stage_diveman_clear    = 1 << 6
+	stage_skullman_clear   = 1 << 7

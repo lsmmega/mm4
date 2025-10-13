712024b7e1e5f7d88c729db9ba398964c69e6260
lsmmega <pogmegalsm@outlook.com>
2025-10-13 12:21:54 +0800

    Update flags.asm

--- constants/flags.asm
+++ constants/flags.asm
@@ -1,12 +1,12 @@
-;rematch flags.
-	rematch_brightman_done  = 1 << 0
-	rematch_toadman_done    = 1 << 1
-	rematch_drillman_done   = 1 << 2
-	rematch_pharaohman_done = 1 << 3
-	rematch_ringman_done    = 1 << 4
-	rematch_dustman_done    = 1 << 5
-	rematch_diveman_done    = 1 << 6
-	rematch_skullman_done   = 1 << 7
+;caslte stage clear flags.
+	cossack_1_clear = 1 << 0
+	cossack_2_clear = 1 << 1
+	cossack_3_clear = 1 << 2
+	cossack_4_clear = 1 << 3
+	wily_1_clear    = 1 << 4
+	wily_2_clear    = 1 << 5
+	wily_3_clear    = 1 << 6
+	wily_4_clear    = 1 << 7
 
 ;regular stage clear flags.
 	stage_brightman_clear  = 1 << 0
@@ -18,12 +18,12 @@
 	stage_diveman_clear    = 1 << 6
 	stage_skullman_clear   = 1 << 7
 
-;caslte stage clear flags.
-	cossack_1_clear = 1 << 0
-	cossack_2_clear = 1 << 1
-	cossack_3_clear = 1 << 2
-	cossack_4_clear = 1 << 3
-	wily_1_clear    = 1 << 4
-	wily_2_clear    = 1 << 5
-	wily_3_clear    = 1 << 6
-	wily_4_clear    = 1 << 7
+;rematch flags.
+	rematch_brightman_done  = 1 << 0
+	rematch_toadman_done    = 1 << 1
+	rematch_drillman_done   = 1 << 2
+	rematch_pharaohman_done = 1 << 3
+	rematch_ringman_done    = 1 << 4
+	rematch_dustman_done    = 1 << 5
+	rematch_diveman_done    = 1 << 6
+	rematch_skullman_done   = 1 << 7

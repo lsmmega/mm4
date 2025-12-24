889a20bce4472a0c4a550e288da759b5c7981b97
lsmmega <pogmegalsm@outlook.com>
2025-12-24 21:32:32 +0800

    split mapset

---
+++ stages/cossack2/cossack2_mapset.asm
@@ -0,0 +1,319 @@
+;cossack2_mapset_00
+	.BYTE $00, $01, $02, $03, $03, $04, $01, $05
+	.BYTE $06, $07, $08, $09, $0A, $0B, $07, $0C
+	.BYTE $0D, $0E, $0E, $08, $0B, $0E, $0E, $0F
+	.BYTE $10, $0E, $11, $12, $12, $13, $0E, $05
+	.BYTE $14, $15, $16, $17, $18, $16, $16, $0C
+	.BYTE $19, $1A, $1A, $1B, $1C, $1A, $1A, $0F
+	.BYTE $1D, $1E, $1D, $1F, $20, $21, $1F, $22
+	.BYTE $23, $24, $21, $25, $22, $22, $25, $22
+
+;cossack2_mapset_01
+	.BYTE $0D, $01, $0F, $1E, $26, $22, $22, $1F
+	.BYTE $10, $01, $1A, $1A, $27, $1A, $1A, $25
+	.BYTE $0D, $1B, $28, $0E, $29, $2A, $0E, $0D
+	.BYTE $10, $0E, $2B, $2C, $0E, $2D, $2E, $2F
+	.BYTE $0D, $0E, $1A, $1A, $0E, $30, $01, $31
+	.BYTE $2F, $0E, $32, $33, $21, $34, $01, $1E
+	.BYTE $35, $2E, $36, $22, $1E, $37, $01, $38
+	.BYTE $39, $3A, $3B, $3C, $3C, $3D, $3A, $3E
+
+;cossack2_mapset_02
+	.BYTE $3F, $21, $1E, $23, $24, $21, $25, $22
+	.BYTE $40, $1A, $1A, $1A, $1A, $1A, $1A, $1A
+	.BYTE $00, $0E, $0E, $3F, $37, $23, $41, $0E
+	.BYTE $06, $2E, $0D, $2B, $1B, $1A, $42, $0E
+	.BYTE $1E, $01, $10, $1E, $0E, $0E, $40, $0E
+	.BYTE $0D, $01, $2F, $41, $0E, $0E, $43, $0E
+	.BYTE $10, $01, $44, $45, $46, $47, $21, $48
+	.BYTE $49, $3A, $4A, $4B, $4C, $4D, $4E, $4F
+
+;cossack2_mapset_03
+	.BYTE $23, $24, $1E, $45, $0E, $0E, $0E, $0E
+	.BYTE $1A, $1A, $1A, $1A, $50, $51, $52, $53
+	.BYTE $0E, $0E, $0E, $50, $54, $55, $56, $57
+	.BYTE $0E, $0E, $0E, $58, $59, $5A, $56, $59
+	.BYTE $0E, $0E, $0E, $5B, $56, $5A, $59, $5C
+	.BYTE $0E, $0E, $0E, $5D, $5C, $56, $5C, $56
+	.BYTE $1F, $1D, $1E, $1D, $5E, $5E, $5F, $5F
+	.BYTE $25, $23, $24, $21, $22, $22, $22, $22
+
+;cossack2_mapset_04
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $0E, $0E, $50, $52, $51, $53, $0E, $0E
+	.BYTE $53, $50, $54, $56, $55, $57, $53, $0E
+	.BYTE $60, $58, $59, $56, $5A, $59, $60, $0E
+	.BYTE $61, $5B, $56, $5C, $56, $5C, $61, $0E
+	.BYTE $62, $5D, $59, $56, $59, $56, $62, $63
+	.BYTE $64, $65, $5E, $5F, $5E, $5F, $64, $66
+	.BYTE $22, $22, $22, $22, $22, $22, $22, $22
+
+;cossack2_mapset_05
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $0E, $50, $52, $51, $53, $0E, $0E, $0E
+	.BYTE $50, $54, $56, $55, $57, $53, $0E, $67
+	.BYTE $58, $59, $56, $5A, $59, $60, $0E, $1A
+	.BYTE $5B, $56, $5C, $56, $5C, $61, $0E, $0E
+	.BYTE $5D, $59, $56, $59, $56, $62, $0E, $0E
+	.BYTE $65, $5E, $5F, $5E, $5F, $64, $66, $66
+	.BYTE $22, $22, $22, $22, $22, $22, $22, $22
+
+;cossack2_mapset_06
+	.BYTE $1A, $1A, $1A, $1A, $1A, $1A, $68, $69
+	.BYTE $0E, $0E, $50, $51, $53, $0E, $6A, $69
+	.BYTE $6B, $50, $54, $56, $57, $53, $1A, $0F
+	.BYTE $1A, $58, $59, $5A, $59, $60, $0E, $05
+	.BYTE $0E, $5B, $56, $5C, $56, $61, $0E, $0C
+	.BYTE $0E, $5D, $59, $59, $56, $62, $0E, $0F
+	.BYTE $66, $6C, $5F, $5E, $5F, $6D, $66, $0D
+	.BYTE $22, $22, $22, $22, $22, $22, $22, $2F
+
+;cossack2_mapset_07
+	.BYTE $0D, $01, $0E, $0E, $0E, $0E, $0E, $05
+	.BYTE $10, $07, $0E, $0E, $0E, $0E, $0E, $0C
+	.BYTE $0D, $0E, $0E, $0E, $0E, $0E, $0E, $0F
+	.BYTE $10, $0E, $0E, $0E, $0E, $0E, $0E, $0F
+	.BYTE $0D, $50, $52, $51, $52, $53, $6E, $69
+	.BYTE $2F, $5B, $59, $55, $59, $60, $68, $6F
+	.BYTE $70, $71, $72, $73, $74, $75, $76, $77
+	.BYTE $78, $79, $78, $78, $79, $78, $7A, $7B
+
+;cossack2_mapset_08
+	.BYTE $0D, $7C, $1A, $1A, $1A, $1A, $1A, $0F
+	.BYTE $10, $07, $0E, $0E, $0E, $0E, $0E, $05
+	.BYTE $0D, $0E, $0E, $0E, $0E, $0E, $0E, $0C
+	.BYTE $10, $0E, $0E, $0E, $0E, $0E, $0E, $0F
+	.BYTE $0D, $0E, $0E, $0E, $0E, $0E, $7D, $7E
+	.BYTE $2F, $0E, $0E, $0E, $0E, $0E, $0E, $0C
+	.BYTE $0D, $2E, $0E, $0E, $0E, $0E, $0E, $05
+	.BYTE $2F, $01, $0E, $0E, $0E, $0E, $0E, $4A
+
+;cossack2_mapset_09
+	.BYTE $23, $7F, $23, $24, $21, $25, $22, $23
+	.BYTE $0D, $1A, $1A, $1A, $1A, $1A, $1A, $1A
+	.BYTE $10, $50, $80, $53, $0E, $0E, $0E, $0E
+	.BYTE $0D, $58, $56, $60, $0E, $28, $81, $82
+	.BYTE $10, $5B, $5A, $61, $0E, $2B, $43, $43
+	.BYTE $0D, $5D, $56, $62, $0E, $83, $84, $3F
+	.BYTE $2F, $85, $21, $86, $86, $87, $88, $89
+	.BYTE $0D, $8A, $3C, $8B, $8B, $4C, $4F, $4C
+
+;cossack2_mapset_0a
+	.BYTE $7F, $23, $23, $7F, $23, $24, $21, $25
+	.BYTE $1A, $1A, $1A, $1A, $1A, $1A, $1A, $1A
+	.BYTE $50, $52, $51, $53, $11, $13, $0E, $0E
+	.BYTE $58, $56, $5C, $60, $0E, $0E, $0E, $0E
+	.BYTE $5B, $56, $56, $61, $0E, $0E, $0E, $8C
+	.BYTE $8D, $3F, $8D, $3F, $8D, $84, $0E, $8E
+	.BYTE $86, $89, $21, $8F, $21, $90, $66, $23
+	.BYTE $4C, $4C, $4C, $4C, $4C, $4C, $4C, $4C
+
+;cossack2_mapset_0b
+	.BYTE $22, $23, $24, $23, $24, $1D, $91, $92
+	.BYTE $1A, $1A, $1A, $1A, $1A, $1A, $91, $25
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $91, $92
+	.BYTE $0E, $32, $84, $0E, $0E, $0E, $1C, $25
+	.BYTE $84, $92, $90, $0E, $0E, $0E, $0E, $23
+	.BYTE $06, $25, $8D, $0E, $0E, $92, $92, $24
+	.BYTE $48, $6F, $1D, $66, $66, $25, $25, $1D
+	.BYTE $4C, $4C, $4C, $4C, $4C, $4C, $4C, $4C
+
+;cossack2_mapset_0c
+	.BYTE $0D, $01, $23, $24, $1D, $1E, $1D, $92
+	.BYTE $10, $01, $1A, $1A, $1A, $1A, $1A, $25
+	.BYTE $0D, $01, $0E, $0E, $0E, $93, $0E, $92
+	.BYTE $10, $01, $92, $41, $1D, $94, $95, $25
+	.BYTE $0D, $1B, $25, $06, $1A, $1A, $91, $92
+	.BYTE $2F, $0E, $96, $96, $15, $84, $91, $25
+	.BYTE $21, $1E, $44, $89, $44, $1D, $91, $44
+	.BYTE $4C, $4C, $4A, $4C, $4A, $8B, $91, $4A
+
+;cossack2_mapset_0d
+	.BYTE $0D, $01, $97, $98, $98, $98, $98, $92
+	.BYTE $10, $01, $99, $15, $17, $32, $9A, $25
+	.BYTE $0D, $01, $9B, $1A, $1B, $9C, $1C, $92
+	.BYTE $10, $01, $9D, $98, $9E, $9F, $9E, $25
+	.BYTE $0D, $01, $A0, $15, $15, $A1, $9A, $92
+	.BYTE $10, $01, $1A, $1A, $1A, $1A, $1C, $25
+	.BYTE $0D, $01, $44, $1D, $44, $1D, $44, $1D
+	.BYTE $2F, $01, $4A, $8B, $4A, $8B, $4A, $8B
+
+;cossack2_mapset_0e
+	.BYTE $0D, $23, $24, $1D, $1E, $1D, $23, $92
+	.BYTE $10, $1A, $1A, $1A, $1A, $1A, $1A, $25
+	.BYTE $0D, $0E, $0E, $0E, $0E, $0E, $A2, $A3
+	.BYTE $10, $0E, $0E, $0E, $0E, $0E, $A4, $A5
+	.BYTE $0D, $0E, $0E, $0E, $0E, $0E, $1C, $A6
+	.BYTE $10, $0E, $0E, $0E, $0E, $0E, $0E, $A7
+	.BYTE $0D, $2E, $44, $1D, $44, $1D, $44, $1D
+	.BYTE $2F, $01, $4A, $8B, $4A, $8B, $4A, $8B
+
+;cossack2_mapset_0f
+	.BYTE $92, $24, $92, $24, $92, $24, $92, $24
+	.BYTE $25, $1D, $25, $1D, $25, $1D, $25, $1D
+	.BYTE $92, $24, $92, $24, $92, $24, $92, $24
+	.BYTE $25, $1D, $25, $1D, $25, $1D, $25, $1D
+	.BYTE $A8, $1A, $1A, $1A, $1A, $1A, $1A, $A6
+	.BYTE $A9, $0E, $0E, $0E, $0E, $0E, $0E, $A7
+	.BYTE $44, $1D, $44, $1D, $44, $1D, $1D, $44
+	.BYTE $4A, $8B, $4A, $8B, $4A, $8B, $8B, $4A
+
+;cossack2_mapset_10
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $21, $1E, $AB, $21, $1E, $AB, $21, $1E
+	.BYTE $AC, $AD, $4B, $AC, $AD, $4B, $AC, $3C
+
+;cossack2_mapset_11
+	.BYTE $AA, $AE, $AF, $B0, $B1, $B2, $B3, $AA
+	.BYTE $AA, $B4, $B5, $B6, $B7, $B8, $B9, $AA
+	.BYTE $AA, $BA, $BB, $BC, $BD, $BE, $BF, $AA
+	.BYTE $AA, $C0, $C1, $C2, $BB, $C3, $C4, $AA
+	.BYTE $AA, $C5, $C6, $C7, $C8, $C9, $CA, $AA
+	.BYTE $AA, $CB, $CC, $CD, $CE, $CF, $D0, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_12
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_13
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_14
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_15
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_16
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_17
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_18
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_19
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_1a
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_1b
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_1c
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_1d
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_1e
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+
+;cossack2_mapset_1f
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
+	.BYTE $AA, $AA, $AA, $AA, $AA, $AA, $AA, $AA
---
+++ stages/cossack3/cossack3_mapset.asm
@@ -0,0 +1,319 @@
+;cossack3_mapset_00
+	.BYTE $00, $01, $02, $03, $04, $05, $05, $05
+	.BYTE $06, $07, $00, $08, $09, $0A, $04, $05
+	.BYTE $0B, $0C, $0D, $0E, $0F, $10, $11, $12
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0C, $0D
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $13, $14, $15, $13, $14, $15, $13, $14
+	.BYTE $16, $17, $18, $16, $17, $18, $16, $17
+
+;cossack3_mapset_01
+	.BYTE $05, $05, $19, $0A, $1A, $1B, $0B, $0F
+	.BYTE $19, $0A, $1C, $1D, $0E, $0B, $0B, $0B
+	.BYTE $1E, $1F, $0E, $20, $0B, $0B, $0B, $21
+	.BYTE $0E, $0B, $0B, $0B, $0B, $0B, $0B, $22
+	.BYTE $0B, $0B, $0B, $0B, $0B, $23, $24, $25
+	.BYTE $0B, $0B, $26, $27, $28, $29, $2A, $2B
+	.BYTE $2C, $0B, $0B, $0B, $2D, $2B, $2B, $2B
+	.BYTE $18, $0B, $0B, $0B, $2E, $2F, $2B, $2B
+
+;cossack3_mapset_02
+	.BYTE $30, $09, $0A, $04, $05, $19, $0A, $1A
+	.BYTE $0C, $0F, $10, $11, $12, $1E, $1F, $0E
+	.BYTE $31, $0B, $0B, $0C, $0D, $32, $33, $0B
+	.BYTE $34, $27, $0B, $35, $33, $0B, $0B, $0B
+	.BYTE $36, $37, $38, $0B, $0B, $0B, $0B, $0B
+	.BYTE $2B, $39, $3A, $3B, $0B, $3C, $0B, $3D
+	.BYTE $2B, $3E, $3F, $40, $0B, $0B, $0B, $0B
+	.BYTE $2B, $41, $42, $43, $0B, $0B, $0B, $0B
+
+;cossack3_mapset_03
+	.BYTE $1F, $0B, $0B, $0B, $0B, $0B, $44, $45
+	.BYTE $20, $0B, $0B, $0B, $0B, $0B, $46, $0B
+	.BYTE $3D, $0B, $3C, $0B, $3D, $0B, $26, $47
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $3D
+	.BYTE $0B, $3C, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $26, $47, $47, $48, $49
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $49, $4A
+
+;cossack3_mapset_04
+	.BYTE $4B, $0B, $0B, $0B, $0B, $0B, $0B, $4C
+	.BYTE $46, $0B, $0B, $0B, $0B, $0B, $4D, $48
+	.BYTE $4E, $0B, $0B, $0B, $0B, $0B, $4F, $0B
+	.BYTE $0B, $50, $0B, $51, $47, $47, $52, $4E
+	.BYTE $0B, $0B, $0B, $53, $54, $55, $56, $0B
+	.BYTE $0B, $3D, $57, $58, $59, $5A, $5B, $5C
+	.BYTE $5D, $5E, $5F, $60, $60, $61, $62, $00
+	.BYTE $63, $64, $60, $60, $60, $60, $61, $65
+
+;cossack3_mapset_05
+	.BYTE $66, $67, $0A, $04, $05, $19, $04, $05
+	.BYTE $0B, $68, $69, $6A, $6B, $6C, $11, $12
+	.BYTE $0B, $0B, $20, $0B, $0B, $0B, $0C, $0D
+	.BYTE $0B, $50, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $6D, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $6E, $0B, $6E, $0B
+	.BYTE $6F, $56, $0B, $3D, $0B, $0B, $0B, $70
+	.BYTE $71, $72, $73, $0B, $0B, $0B, $0B, $0B
+
+;cossack3_mapset_06
+	.BYTE $19, $0A, $1A, $1F, $0B, $0B, $74, $17
+	.BYTE $1E, $1F, $0E, $20, $0B, $0B, $74, $17
+	.BYTE $0E, $0B, $0B, $0B, $0B, $0B, $74, $75
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $74, $17
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $76, $17
+	.BYTE $0B, $0B, $54, $77, $0B, $0B, $78, $17
+	.BYTE $79, $7A, $7B, $7C, $7D, $79, $7E, $7F
+	.BYTE $57, $80, $60, $60, $81, $82, $73, $0B
+
+;cossack3_mapset_07
+	.BYTE $17, $83, $0B, $0B, $0B, $0B, $78, $84
+	.BYTE $17, $83, $85, $0B, $0B, $0B, $78, $17
+	.BYTE $86, $87, $0D, $0B, $54, $77, $78, $86
+	.BYTE $88, $87, $0B, $0B, $0F, $10, $78, $17
+	.BYTE $89, $4E, $0B, $0B, $0B, $0B, $78, $17
+	.BYTE $17, $8A, $51, $48, $51, $48, $78, $75
+	.BYTE $86, $8B, $8C, $8D, $0B, $0B, $8E, $17
+	.BYTE $8F, $90, $91, $92, $0B, $0B, $74, $17
+
+;cossack3_mapset_08
+	.BYTE $17, $0B, $66, $67, $0A, $04, $05, $19
+	.BYTE $17, $0B, $0B, $68, $69, $93, $94, $6C
+	.BYTE $17, $0B, $0B, $0B, $20, $0B, $0B, $0B
+	.BYTE $17, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $17, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $75, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $17, $95, $96, $97, $97, $96, $98, $99
+	.BYTE $17, $83, $0B, $0B, $0B, $0B, $78, $9A
+
+;cossack3_mapset_09
+	.BYTE $04, $05, $19, $9B, $1A, $1F, $0B, $0B
+	.BYTE $11, $12, $1E, $1F, $0E, $20, $0B, $0B
+	.BYTE $0C, $0D, $0E, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $26, $27, $0B, $0B, $0B, $0B
+	.BYTE $70, $9C, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $9D, $5D, $9E, $54
+	.BYTE $0B, $0B, $0B, $9D, $9F, $63, $A0, $A1
+	.BYTE $0B, $0B, $49, $9F, $60, $60, $60, $60
+
+;cossack3_mapset_0a
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $8D, $0B, $26, $96, $27, $0B, $3D, $0B
+	.BYTE $A2, $8A, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $61, $A3, $8D, $0B, $0B, $0B, $0B, $0B
+
+;cossack3_mapset_0b
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0F, $30, $09
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0C, $0F
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $21, $31, $0B, $0B
+	.BYTE $70, $9C, $0B, $0B, $22, $A4, $0B, $0B
+	.BYTE $0B, $0B, $A5, $A6, $25, $36, $37, $38
+	.BYTE $0B, $28, $29, $2A, $2B, $2B, $39, $3A
+	.BYTE $0B, $2D, $2B, $2B, $2B, $2B, $3E, $3F
+
+;cossack3_mapset_0c
+	.BYTE $0A, $04, $05, $19, $0A, $1A, $1F, $0B
+	.BYTE $10, $11, $12, $1E, $1F, $0E, $20, $0B
+	.BYTE $0B, $0C, $0D, $0E, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $3C, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $70, $33, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $3B, $0B, $0B, $0B, $0B, $0B, $0B, $49
+	.BYTE $40, $0B, $0B, $0B, $0B, $0B, $49, $4A
+
+;cossack3_mapset_0d
+	.BYTE $0B, $0B, $0B, $0B, $0F, $30, $09, $0A
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0C, $0F, $10
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $70, $9C, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $20, $0B, $0B, $0B, $0B, $0B
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.BYTE $A7, $A8, $A9, $8D, $0B, $0B, $0B, $0B
+	.BYTE $63, $A0, $A1, $A2, $8D, $0B, $0B, $0B
+
+;cossack3_mapset_0e
+	.BYTE $17, $17, $17, $17, $17, $17, $17, $AA
+	.BYTE $17, $17, $17, $17, $17, $17, $AB, $AC
+	.BYTE $17, $17, $17, $17, $17, $AB, $AD, $AE
+	.BYTE $AF, $B0, $47, $B1, $B2, $AD, $B3, $B4
+	.BYTE $B5, $B6, $79, $79, $9C, $B7, $B8, $B9
+	.BYTE $0B, $0B, $0B, $0B, $0B, $BA, $BB, $17
+	.BYTE $BC, $14, $15, $13, $14, $15, $17, $17
+	.BYTE $16, $17, $18, $16, $17, $18, $17, $17
+
+;cossack3_mapset_0f
+	.BYTE $17, $17, $17, $17, $17, $17, $17, $17
+	.BYTE $B2, $B2, $B2, $B2, $B2, $B2, $B2, $B2
+	.BYTE $BD, $B3, $B3, $B3, $B3, $B3, $B3, $AE
+	.BYTE $BE, $B8, $B8, $B8, $B8, $B8, $B8, $B4
+	.BYTE $BF, $C0, $C1, $C1, $C1, $C0, $C1, $C1
+	.BYTE $C2, $C3, $C4, $C2, $C3, $C4, $C3, $C4
+	.BYTE $C5, $17, $C6, $C5, $17, $C6, $17, $C6
+	.BYTE $17, $17, $17, $17, $17, $17, $17, $17
+
+;cossack3_mapset_10
+	.BYTE $C7, $C0, $C0, $C0, $C0, $C0, $C0, $C8
+	.BYTE $C9, $B3, $B3, $B3, $B3, $B3, $B3, $CA
+	.BYTE $CB, $B8, $B8, $B8, $B8, $B8, $B8, $CC
+	.BYTE $BE, $B8, $B8, $B8, $B8, $B8, $B8, $CD
+	.BYTE $CE, $CF, $D0, $D1, $D0, $D1, $D2, $D3
+	.BYTE $D4, $B8, $B8, $B8, $B8, $B8, $B8, $CC
+	.BYTE $D5, $D6, $D6, $D6, $D6, $D6, $D6, $D7
+	.BYTE $D8, $D9, $D9, $D9, $D9, $D9, $D9, $DA
+
+;cossack3_mapset_11
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_12
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_13
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_14
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_15
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_16
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_17
+	.BYTE $19, $0A, $1A, $1F, $0B, $0B, $74, $17
+	.BYTE $1E, $1F, $0E, $20, $0B, $0B, $74, $17
+	.BYTE $0E, $0B, $0B, $0B, $0B, $0B, $74, $75
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $74, $17
+	.BYTE $0B, $0B, $0B, $0B, $0B, $0B, $76, $17
+	.BYTE $0B, $0B, $DB, $DC, $0B, $0B, $78, $17
+	.BYTE $79, $7A, $7B, $7C, $7D, $79, $7E, $7F
+	.BYTE $57, $80, $60, $60, $81, $82, $73, $0B
+
+;cossack3_mapset_18
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_19
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_1a
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_1b
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_1c
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_1d
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_1e
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+
+;cossack3_mapset_1f
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.BYTE $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F

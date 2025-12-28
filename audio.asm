237ab6c464b50c489b3a7906eb27fb82e802415b
lsmmega <pogmegalsm@outlook.com>
2025-12-28 23:17:19 +0800

    sfx charge shot charging 1 done

--- audio.asm
+++ audio.asm
@@ -36,6 +36,7 @@
 .INCLUDE "audio/sfx/route.asm"
 .INCLUDE "audio/sfx/jump_into_water.asm"
 .INCLUDE "audio/sfx/1up.asm"
+.INCLUDE "audio/sfx/charge_shot_charging_1.asm"
 .INCBIN  "audio/_audio_0.bin"
 .INCLUDE "audio/sfx/appearing_block.asm"
 .INCLUDE "audio/music/get_item.asm"
---
+++ audio/sfx/charge_shot_charging_1.asm
@@ -0,0 +1,18 @@
+sfx_charge_shot_charging_1:
+	sfx_priority 7
+
+;1
+	current_sfx_global_transpose .SET 0
+	sfx_global_channel_flags sfx_ads_set
+	sfx_ads $FF
+	sfx_frames 12
+	.BYTE sfx_pulse_2
+
+;sfx_pulse_2
+	sfx_channel_flags sfx_instruments_set | sfx_duty_cycle_set | sfx_volume_set | sfx_pitch_slide_set
+	sfx_instruments 60
+	sfx_duty_cycle 1
+	sfx_volume 15
+	sfx_pitch_slide $8E
+	sfx_note Ab3
+	sfx_end
